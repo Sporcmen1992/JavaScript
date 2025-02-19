2696e73cc9e946302ff5028a633e9c5cb8a053f5
Sporcmen1992 <sporcmen1992@gmail.com>
2025-02-19 21:05:45 +0200

    JavaScript

---
+++ script.js
@@ -0,0 +1,32 @@
+// Task Nr1
+let string = "Labas";
+let number = 32;
+let boolean = true;
+
+console.log(string, number, boolean);
+
+// Task Nr2
+let age = 18;
+let isCitizen = true;
+let isResidentFor = 33;
+
+if(age >= 18 && isCitizen && isResidentFor >= 30) {
+    console.log("You're allowed to vote");
+} else if(age < 18 && isCitizen && isResidentFor < 30 ) {
+    console.log("You're allowed to vote");
+} else if(!isCitizen){
+    console.log("You're not allowed to vote");
+} else {
+    console.log("You're not allowed to vote");
+}
+
+// Task Nr3
+let age1 = 30;
+let bmi = 50;
+let smoke = true;
+
+if(age1 < 30 && bmi < 25 && !smoke) {
+    console.log("Low risk");
+} else if()
+
+
